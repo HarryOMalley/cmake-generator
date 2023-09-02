a4bdcabaa296f41ddc3eb32162e0ec751ab10115
Harry <homalle1@jaguarlandrover.com>
2023-09-02 20:02:48 +0100

    Implement cmake_generator logic
    Add cmake_generator function
    Add logic to list files in the extension

--- src/cmake_generator.ts
+++ src/cmake_generator.ts
@@ -3,3 +3,203 @@
  * @description This module contains the logic to generate a CMakeLists.txt file
  */
 
+import path = require("path");
+
+
+export enum ILibraryType {
+    STATIC = "STATIC",
+    SHARED = "SHARED",
+    INTERFACE = "INTERFACE"
+}
+
+export enum ISourceVisibility {
+    PRIVATE = "PRIVATE",
+    PUBLIC = "PUBLIC",
+    INTERFACE = "INTERFACE"
+}
+
+export enum ISourceType {
+    REGULAR,  // Regular source files like .cpp, .c, etc.
+    HEADER,   // Header files, primarily for C and C++.
+    CXX_MODULE // Experimental C++ Modules, only if you wish to support them.
+}
+
+export type ISourceFile = {
+    name: string;
+    path: string;
+};
+
+export type ISources = {
+    files: ISourceFile[];
+    type: ISourceType;
+    visibility: ISourceVisibility;
+};
+
+export type ILibrary = {
+    name: string;
+    type: ILibraryType;
+    sources: ISources[];
+};
+
+export function createLibrary(library: ILibrary) {
+    // Generate the CMake command to add a library
+    let cmakeCommand = `add_library(${library.name} ${library.type})\n`;
+
+    // Loop over each source group
+    for (const sources of library.sources) {
+
+        let sourceType: string = "";
+        // Determine the type of the source group
+        let groupType: ISourceType = sources.type;
+        switch (groupType) {
+            case ISourceType.REGULAR:
+                sourceType = " "
+                break;
+            case ISourceType.HEADER:
+                sourceType = `FILE_SET HEADERS FILES `;
+                break;
+            case ISourceType.CXX_MODULE:
+                sourceType = `FILE_SET CXX_MODULES FILES `;
+                break;
+        }
+        let targetSourcesCommand = `\ntarget_sources(${library.name} ${sources.visibility} ${sourceType}${sources.files.map((file) => file.path).join(" ")})`;
+
+        cmakeCommand += targetSourcesCommand;
+    }
+
+    return cmakeCommand;
+}
+
+
+
+
+/**
+ * 
+ macro(create_library LIB_NAME)
+    # Define options and defaults
+    set(options
+        SHARED
+        STATIC
+        INTERFACE
+        INSTALL
+    )
+    set(oneValueArgs HEADER_LOCATION HEADER_INSTALL_LOCATION)
+    set(multiValueArgs
+        LINK_LIBRARIES_PUBLIC
+        LINK_LIBRARIES_PRIVATE
+        SOURCE_FILES
+        INCLUDE_DIRECTORIES_PUBLIC
+        INCLUDE_DIRECTORIES_PRIVATE
+    )
+
+    # Parse the arguments provided to the macro
+    cmake_parse_arguments(
+        LIB_ARG
+        "${options}"
+        "${oneValueArgs}"
+        "${multiValueArgs}"
+        ${ARGN}
+    )
+
+    # Set default header location if not provided
+    if(NOT LIB_ARG_HEADER_LOCATION)
+        set(LIB_ARG_HEADER_LOCATION "${CMAKE_CURRENT_SOURCE_DIR}/include")
+    endif()
+
+    # Set default header install location if not provided
+    if(NOT LIB_ARG_HEADER_INSTALL_LOCATION)
+        set(LIB_ARG_HEADER_INSTALL_LOCATION "include/${LIB_NAME}")
+    endif()
+
+    # Determine the type of the library (SHARED, STATIC, INTERFACE)
+    set(LIB_TYPE)
+    if(${LIB_ARG_SHARED})
+        set(LIB_TYPE SHARED)
+    elseif(${LIB_ARG_STATIC})
+        set(LIB_TYPE STATIC)
+    elseif(${LIB_ARG_INTERFACE})
+        set(LIB_TYPE INTERFACE)
+    else()
+        message(FATAL_ERROR "Failed to create ${LIB_NAME}. You must specify a library type: SHARED, STATIC, or INTERFACE.")
+    endif()
+
+    # Create a library with the determined type
+    add_library(${LIB_NAME} ${LIB_TYPE})
+
+    # Configure the library based on its type
+    if(${LIB_TYPE} STREQUAL "INTERFACE")
+        target_include_directories(
+            ${LIB_NAME}
+            INTERFACE $<BUILD_INTERFACE:${LIB_ARG_HEADER_LOCATION}>
+                      $<INSTALL_INTERFACE:${LIB_ARG_HEADER_INSTALL_LOCATION}>
+        )
+
+        if(LIB_ARG_INCLUDE_DIRECTORIES_PUBLIC)
+            target_include_directories(${LIB_NAME} INTERFACE ${LIB_ARG_INCLUDE_DIRECTORIES_PUBLIC})
+        endif()
+
+        if(LIB_ARG_LINK_LIBRARIES_PUBLIC)
+            target_link_libraries(${LIB_NAME} INTERFACE ${LIB_ARG_LINK_LIBRARIES_PUBLIC})
+        endif()
+    else()
+        target_sources(${LIB_NAME} PRIVATE ${LIB_ARG_SOURCE_FILES})
+
+        if(LIB_ARG_LINK_LIBRARIES_PUBLIC)
+            target_link_libraries(${LIB_NAME} PUBLIC ${LIB_ARG_LINK_LIBRARIES_PUBLIC})
+        endif()
+
+        if(LIB_ARG_LINK_LIBRARIES_PRIVATE)
+            target_link_libraries(${LIB_NAME} PRIVATE ${LIB_ARG_LINK_LIBRARIES_PRIVATE})
+        endif()
+
+        target_include_directories(
+            ${LIB_NAME} 
+            PUBLIC $<BUILD_INTERFACE:${LIB_ARG_HEADER_LOCATION}>
+                   $<INSTALL_INTERFACE:${LIB_ARG_HEADER_INSTALL_LOCATION}>
+        )
+
+        if(LIB_ARG_INCLUDE_DIRECTORIES_PUBLIC)
+            target_include_directories(${LIB_NAME} PUBLIC ${LIB_ARG_INCLUDE_DIRECTORIES_PUBLIC})
+        endif()
+
+        if(LIB_ARG_INCLUDE_DIRECTORIES_PRIVATE)
+            target_include_directories(${LIB_NAME} PRIVATE ${LIB_ARG_INCLUDE_DIRECTORIES_PRIVATE})
+        endif()
+    endif()
+
+    # Glob the header files in the provided location
+    file(
+        GLOB
+        HEADER_FILES
+        "${LIB_ARG_HEADER_LOCATION}/*.h"
+        "${LIB_ARG_HEADER_LOCATION}/*.hpp"
+    )
+
+    # Install the library if the INSTALL option is specified
+    if(${LIB_ARG_INSTALL})
+        set_target_properties(${LIB_NAME} PROPERTIES PUBLIC_HEADER "${HEADER_FILES}")
+        if(${LIB_TYPE} STREQUAL "INTERFACE")
+            install(
+                TARGETS ${LIB_NAME}
+                EXPORT ${LIB_NAME}Targets # this exports the targets
+                PUBLIC_HEADER DESTINATION ${LIB_ARG_HEADER_INSTALL_LOCATION}
+            )
+        else()
+            install(
+                TARGETS ${LIB_NAME}
+                EXPORT ${LIB_NAME}Targets # this exports the targets
+                LIBRARY DESTINATION lib
+                PUBLIC_HEADER DESTINATION ${LIB_ARG_HEADER_INSTALL_LOCATION}
+            )
+        endif()
+
+        # Export the targets to a CMake file for easy inclusion in other projects
+        install(
+            EXPORT ${LIB_NAME}Targets
+            FILE ${LIB_NAME}Targets.cmake
+            NAMESPACE ${LIB_NAME}::
+            DESTINATION lib/cmake/${LIB_NAME}
+        )
+    endif()
+endmacro()
+ */
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,93 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { ILibrary, ILibraryType, ISourceType, ISourceVisibility, createLibrary } from './cmake_generator';
+import * as fs from 'fs';
+import * as path from 'path';
+
+async function showFilePickerQuickPick(): Promise<string | undefined> {
+	let currentDir: string | undefined;
+
+	const workspaceFolders = vscode.workspace.workspaceFolders;
+
+	// 1. Check if there's an open workspace.
+	if (workspaceFolders) {
+		currentDir = workspaceFolders[0].uri.fsPath;
+		console.log("currentDir: " + currentDir);
+	} else {
+		console.log("No workspace folders found.")
+		// 2. Check the directory of the active file.
+		const activeEditor = vscode.window.activeTextEditor;
+		if (activeEditor && activeEditor.document.uri.scheme === 'file') {
+			currentDir = path.dirname(activeEditor.document.uri.fsPath);
+			console.log("currentDir: " + currentDir);
+		} else {
+			console.log("No active editor found.")
+			// 3. Prompt the user to choose a base directory.
+			const userChosenDir = await vscode.window.showOpenDialog({
+				canSelectFiles: false,
+				canSelectFolders: true,
+				canSelectMany: false,
+				title: 'Select a base directory'
+			});
+
+			if (userChosenDir && userChosenDir[0]) {
+				currentDir = userChosenDir[0].fsPath;
+			} else {
+				vscode.window.showErrorMessage("Failed to select a directory.");
+				return;
+			}
+		}
+	}
+
+	while (true) {
+		if (!fs.existsSync(currentDir)) {
+			console.error(`Directory does not exist: ${currentDir}`);
+			vscode.window.showErrorMessage(`Directory does not exist: ${currentDir}`);
+			return;
+		}
+
+		let entries: string[] = [];
+		try {
+			const directoryEntries = fs.readdirSync(currentDir, { withFileTypes: true });
+
+			const directories = directoryEntries.filter(de => de.isDirectory()).map(de => de.name).sort();
+			const files = directoryEntries.filter(de => !de.isDirectory()).map(de => de.name).sort();
+
+			entries = ['..', ...directories, ...files];
+			console.log("entries: " + entries);
+		} catch (error) {
+			vscode.window.showErrorMessage(`Error reading directory: ${(error as Error).message}`);
+			return;
+		}
+
+		const pick = await vscode.window.showQuickPick(entries, {
+			placeHolder: 'Select a file or directory',
+		});
+
+		if (!pick) {
+			console.log("User canceled the quick pick");
+			return; // User canceled the quick pick
+		}
+
+		if (pick === '..') {
+			// Go up one directory
+			currentDir = path.dirname(currentDir);
+		} else {
+			const newDir = path.join(currentDir, pick);
+
+			const stats = fs.statSync(newDir);
+			if (stats.isDirectory()) {
+				currentDir = newDir;
+			} else if (stats.isFile()) {
+				return newDir;
+			}
+		}
+	}
+}
+
+
+
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -13,9 +100,117 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('cmake-generator.create-library', () => {
-		vscode.window.showInformationMessage('Creating a CMake Library!');
+	let disposable = vscode.commands.registerCommand('cmake-generator.create-library', async () => {
+		const initialPath = vscode.workspace.workspaceFolders ? vscode.workspace.workspaceFolders[0].uri.fsPath : '.';
+		const editor = vscode.window.activeTextEditor;
 
+		if (editor) {
+			const documentPath = editor.document.uri.fsPath;
+			const docDir = path.dirname(documentPath);
+			// Usage example
+			showFilePickerQuickPick().then(file => {
+				if (file) {
+					console.log(`Selected file: ${file}`);
+				} else {
+					console.log('No file was selected.');
+				}
+			});
+
+			// if (selectedPath) {
+			// 	vscode.window.showInformationMessage(`Selected file: ${selectedPath}`);
+			// }
+		} else {
+			vscode.window.showErrorMessage('No active editor found.');
+		}
+		vscode.window.showInformationMessage('Creating a CMake Library!');
+		// Example Usage:
+		const libraryName = await vscode.window.showInputBox({
+			prompt: 'Enter the library name',
+			placeHolder: 'Name here'
+		});
+
+		if (!libraryName) {
+			vscode.window.showErrorMessage('Library name is required');
+			return;
+		}
+
+		let libraryTypes: ILibraryType[] = Object.values(ILibraryType);
+
+		// Show quick pick to the user
+		const libraryTypeItems: vscode.QuickPickItem[] = libraryTypes.map((type: ILibraryType) => {
+			return { label: type.toString(), description: '' };
+		});
+
+		const selectedLibraryType: vscode.QuickPickItem | undefined = await vscode.window.showQuickPick(libraryTypeItems, {
+			placeHolder: 'Select the library type',
+		});
+
+		if (!selectedLibraryType) {
+			vscode.window.showErrorMessage('Library type is required');
+			return;
+		}
+
+		const libraryType: ILibraryType = selectedLibraryType.label as ILibraryType;
+
+		if (!libraryType) {
+			vscode.window.showErrorMessage('Library type is required');
+			return;
+		}
+
+		vscode.window.showInformationMessage(`Creating ${libraryName}! Library type: ${libraryType}`);
+
+		const options: vscode.OpenDialogOptions = {
+			canSelectMany: true,      // Allows selecting multiple files.
+			openLabel: 'Open',        // Label for the open button.
+			filters: {
+				'Text files': ['txt'], // Filter files by extension (in this case, .txt files).
+				'All files': ['*']     // Allow selecting any type of file.
+			}
+		};
+
+		// Show the file picker.
+		const fileUris = await vscode.window.showOpenDialog(options);
+
+		if (fileUris && fileUris.length > 0) {
+			for (const fileUri of fileUris) {
+				vscode.window.showInformationMessage(`Selected file: ${fileUri.fsPath}`);
+			}
+		}
+
+
+		const exampleLibrary: ILibrary = {
+			name: libraryName,
+			type: libraryType,
+			sources: [
+				{
+					type: ISourceType.REGULAR,
+					visibility: ISourceVisibility.PRIVATE,
+					files: [
+						{
+							name: "source1",
+							path: "src/source1.cpp",
+
+						}
+					]
+				},
+				{
+					type: ISourceType.HEADER,
+					visibility: ISourceVisibility.PUBLIC,
+					files: [
+						{
+							name: "header1",
+							path: "include/header1.hpp",
+						},
+						{
+							name: "header2",
+							path: "include/header2.hpp",
+						}
+					]
+				}
+			]
+		};
+
+		console.log(createLibrary(exampleLibrary));
 	});
 
 	context.subscriptions.push(disposable);
